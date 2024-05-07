f9c2f6e745289b2291230516080b580f65fa18f3
Hanna Lackner <hlackner@schueler.hakzell.at>
2024-05-07 09:41:18 +0200

    add library

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hannalackner/demowetter',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c90b6669e21e9a12363cc527fac7ea12c18d71f1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hannalackner/demowetter' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c90b6669e21e9a12363cc527fac7ea12c18d71f1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
